e78995cb318d0345962513c758e0c6158cf32cf2
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2024-11-18 22:01:37 +0100

    refactor: item class update

--- js/game.js
+++ js/game.js
@@ -11,118 +11,144 @@ let lpsLost = document.getElementById("lps-lost");
 
 class Item {
   constructor(
-    itemButton,
-    itemList,
-    itemDisplay,
-    itemTotal,
-    itemCost,
-    itemBuyFactor,
-    itemMultiplier,
-    itemModule,
-    itemMax,
-    itemValue
+    buttonDisplay,
+    listDisplay,
+    imageDisplay,
+    amountDisplay,
+    cost,
+    costDisplay,
+    buyFactor,
+    multiplierStep,
+    objective,
+    value
   ) {
-    this.itemButton = itemButton; //DOM
-    this.itemList = itemList; //DOM
-    this.itemDisplay = itemDisplay; //DOM
-    this.itemTotal = itemTotal; //DOM
-    this.itemCost = itemCost; //DOM
-    this.itemBuyFactor = itemBuyFactor;
-    this.itemMultiplier = itemMultiplier;
-    this.itemModule = itemModule;
-    this.itemMax = itemMax;
-    this.itemValue = itemValue;
+    this.buttonDisplay = buttonDisplay;
+    this.listDisplay = listDisplay;
+    this.imageDisplay = imageDisplay;
+    this.amount = 0;
+    this.amountDisplay = amountDisplay;
+    this.cost = cost;
+    this.costDisplay = costDisplay;
+    this.buyFactor = buyFactor;
+    this.multiplier = 1;
+    this.multiplierStep = multiplierStep;
+    this.objective = objective;
+    this.value = value;
   }
 
-  itemButtonInit(totalLove) {
-    this.itemButton.addEventListener("click", () => {
-      if (totalLove.innerText >= Number(this.itemCost.innerText)) {
-        this.itemDisplay.style.visibility = "visible";
-        this.itemTotal.innerText = Number(this.itemTotal.innerText) + 1;
-        totalLove.innerText =
-          Number(totalLove.innerText) - Number(this.itemCost.innerText);
-        this.itemCost.innerText = Math.round(
-          10 * Number(this.itemBuyFactor) ** Number(this.itemTotal.innerText)
-        );
-        if (Number(this.itemTotal.innerText) % this.itemModule === 0) {
-          this.itemMultiplier += 1;
-        }
+  buy(totalLove) {
+    if (totalLove >= this.cost) {
+      if (this.imageDisplay) {
+        this.imageDisplay.style.visibility = "visible";
       }
-    });
+      this.amount += 1;
+      totalLove -= this.cost;
+      this.cost = Math.round(10 * this.buyFactor ** this.amount);
+      if (this.amount % this.multiplierStep === 0) {
+        this.multiplier++;
+      }
+      this.updateDOM();
+    }
+    return totalLove;
   }
 
-  itemStyleUpdate(totalLove) {
-    if (Number(this.itemTotal.innerText) >= this.itemMax) {
-      this.itemList.style.textDecoration = "line-through";
+  updateStyle(totalLove) {
+    if (this.amount >= this.objective) {
+      this.listDisplay.style.textDecoration = "line-through";
     }
 
-    if (Number(totalLove.innerText) >= Number(this.itemCost.innerText)) {
-      this.itemButton.style.backgroundColor = "yellowgreen";
-    } else {
-      this.itemButton.style.backgroundColor = "silver";
-    }
+    totalLove >= this.cost
+      ? (this.buttonDisplay.style.backgroundColor = "yellowgreen")
+      : (this.buttonDisplay.style.backgroundColor = "silver");
   }
 
-  itemLoveValue() {
-    return (
-      Math.round(this.itemTotal.innerText * this.itemValue) *
-      this.itemMultiplier
-    );
+  updateDOM() {
+    this.amountDisplay.innerText = this.amount;
+    this.costDisplay.innerText = this.cost;
+  }
+
+  getLoveValue() {
+    return Math.round(this.amount * this.value) * this.multiplier;
   }
 }
 
-// Variables AGUA
-const buyWater = document.getElementById("water-button");
-const waterList = document.getElementById("water-list");
-const waterDisplay = document.getElementById("water-display");
-let totalWater = document.getElementById("water-total");
-let waterCost = document.getElementById("water-price");
-let waterBuyFactor = 1.07;
-let waterMultiplier = 1;
+const water = new Item(
+  document.getElementById("water-button"),
+  document.getElementById("water-list"),
+  document.getElementById("water-display"),
+  document.getElementById("water-total"),
+  10,
+  document.getElementById("water-price"),
+  1.07,
+  25,
+  90,
+  1
+);
 
-// Variables COMIDA
-const buyFood = document.getElementById("food-button");
-const foodList = document.getElementById("food-list");
-const foodDisplay = document.getElementById("food-display");
-let totalFood = document.getElementById("food-total");
-let foodCost = document.getElementById("food-price");
-let foodBuyFactor = 1.14;
-let foodMultiplier = 1;
+const food = new Item(
+  document.getElementById("food-button"),
+  document.getElementById("food-list"),
+  document.getElementById("food-display"),
+  document.getElementById("food-total"),
+  50,
+  document.getElementById("food-price"),
+  1.14,
+  15,
+  40,
+  2
+);
 
-// Variables CAMAS
-const buyBed = document.getElementById("bed-button");
-const bedList = document.getElementById("bed-list");
-const bedDisplay = document.getElementById("bed-display");
-let totalBed = document.getElementById("bed-total");
-let bedCost = document.getElementById("bed-price");
-let bedBuyFactor = 1.12;
-let bedMultiplier = 1;
+const beds = new Item(
+  document.getElementById("bed-button"),
+  document.getElementById("bed-list"),
+  document.getElementById("bed-display"),
+  document.getElementById("bed-total"),
+  100,
+  document.getElementById("bed-price"),
+  1.12,
+  10,
+  35,
+  3
+);
 
-// Variables JUGUETES
-const buyToy = document.getElementById("toy-button");
-const toyList = document.getElementById("toy-list");
-const toyDisplay = document.getElementById("toy-display");
-let totalToy = document.getElementById("toy-total");
-let toyCost = document.getElementById("toy-price");
-let toyBuyFactor = 1.1;
-let toyMultiplier = 1;
+const toys = new Item(
+  document.getElementById("toy-button"),
+  document.getElementById("toy-list"),
+  document.getElementById("toy-display"),
+  document.getElementById("toy-total"),
+  500,
+  document.getElementById("toy-price"),
+  1.1,
+  5,
+  20,
+  4
+);
 
-// Variables ACCESORIOS
-const buyAccessory = document.getElementById("accessory-button");
-const accessoryList = document.getElementById("accessory-list");
-let totalAccessory = document.getElementById("accessory-total");
-let accessoryCost = document.getElementById("accessory-price");
-let accessoryBuyFactor = 1.06;
-let accessoryMultiplier = 1;
+const accessories = new Item(
+  document.getElementById("accessory-button"),
+  document.getElementById("accessory-list"),
+  false,
+  document.getElementById("accessory-total"),
+  1000,
+  document.getElementById("accessory-price"),
+  1.06,
+  10,
+  25,
+  5
+);
 
-// Variables CHUCHES
-const buyTreat = document.getElementById("treat-button");
-const treatList = document.getElementById("treat-list");
-const treatDisplay = document.getElementById("treat-display");
-let totalTreat = document.getElementById("treat-total");
-let treatCost = document.getElementById("treat-price");
-let treatBuyFactor = 1.02;
-let treatMultiplier = 1;
+const treats = new Item(
+  document.getElementById("treat-button"),
+  document.getElementById("treat-list"),
+  document.getElementById("treat-display"),
+  document.getElementById("treat-total"),
+  5000,
+  document.getElementById("treat-price"),
+  1.02,
+  5,
+  15,
+  6
+);
 
 // FUNCIONES
 function addToLove(amount) {
@@ -182,77 +208,77 @@ function playGame() {
   }
 }
 
-function waterStyleUpdate() {
-  if (Number(totalWater.innerText) >= 90) {
-    waterList.style.textDecoration = "line-through";
-  }
+// function waterStyleUpdate() {
+//   if (Number(totalWater.innerText) >= 90) {
+//     waterList.style.textDecoration = "line-through";
+//   }
 
-  if (Number(totalLove.innerText) >= Number(waterCost.innerText)) {
-    buyWater.style.backgroundColor = "yellowgreen";
-  } else {
-    buyWater.style.backgroundColor = "silver";
-  }
-}
+//   if (Number(totalLove.innerText) >= Number(waterCost.innerText)) {
+//     buyWater.style.backgroundColor = "yellowgreen";
+//   } else {
+//     buyWater.style.backgroundColor = "silver";
+//   }
+// }
 
-function foodStyleUpdate() {
-  if (Number(totalFood.innerText) >= 40) {
-    foodList.style.textDecoration = "line-through";
-  }
+// function foodStyleUpdate() {
+//   if (Number(totalFood.innerText) >= 40) {
+//     foodList.style.textDecoration = "line-through";
+//   }
 
-  if (Number(totalLove.innerText) >= Number(foodCost.innerText)) {
-    buyFood.style.backgroundColor = "yellowgreen";
-  } else {
-    buyFood.style.backgroundColor = "silver";
-  }
-}
+//   if (Number(totalLove.innerText) >= Number(foodCost.innerText)) {
+//     buyFood.style.backgroundColor = "yellowgreen";
+//   } else {
+//     buyFood.style.backgroundColor = "silver";
+//   }
+// }
 
-function bedStyleUpdate() {
-  if (Number(totalBed.innerText) >= 35) {
-    bedList.style.textDecoration = "line-through";
-  }
+// function bedStyleUpdate() {
+//   if (Number(totalBed.innerText) >= 35) {
+//     bedList.style.textDecoration = "line-through";
+//   }
 
-  if (Number(totalLove.innerText) >= Number(bedCost.innerText)) {
-    buyBed.style.backgroundColor = "yellowgreen";
-  } else {
-    buyBed.style.backgroundColor = "silver";
-  }
-}
+//   if (Number(totalLove.innerText) >= Number(bedCost.innerText)) {
+//     buyBed.style.backgroundColor = "yellowgreen";
+//   } else {
+//     buyBed.style.backgroundColor = "silver";
+//   }
+// }
 
-function toyStyleUpdate() {
-  if (Number(totalToy.innerText) >= 20) {
-    toyList.style.textDecoration = "line-through";
-  }
+// function toyStyleUpdate() {
+//   if (Number(totalToy.innerText) >= 20) {
+//     toyList.style.textDecoration = "line-through";
+//   }
 
-  if (Number(totalLove.innerText) >= Number(toyCost.innerText)) {
-    buyToy.style.backgroundColor = "yellowgreen";
-  } else {
-    buyToy.style.backgroundColor = "silver";
-  }
-}
+//   if (Number(totalLove.innerText) >= Number(toyCost.innerText)) {
+//     buyToy.style.backgroundColor = "yellowgreen";
+//   } else {
+//     buyToy.style.backgroundColor = "silver";
+//   }
+// }
 
-function accessoryStyleUpdate() {
-  if (Number(totalAccessory.innerText) >= 25) {
-    accessoryList.style.textDecoration = "line-through";
-  }
+// function accessoryStyleUpdate() {
+//   if (Number(totalAccessory.innerText) >= 25) {
+//     accessoryList.style.textDecoration = "line-through";
+//   }
 
-  if (Number(totalLove.innerText) >= Number(accessoryCost.innerText)) {
-    buyAccessory.style.backgroundColor = "yellowgreen";
-  } else {
-    buyAccessory.style.backgroundColor = "silver";
-  }
-}
+//   if (Number(totalLove.innerText) >= Number(accessoryCost.innerText)) {
+//     buyAccessory.style.backgroundColor = "yellowgreen";
+//   } else {
+//     buyAccessory.style.backgroundColor = "silver";
+//   }
+// }
 
-function treatStyleUpdate() {
-  if (Number(totalTreat.innerText) >= 15) {
-    treatList.style.textDecoration = "line-through";
-  }
+// function treatStyleUpdate() {
+//   if (Number(totalTreat.innerText) >= 15) {
+//     treatList.style.textDecoration = "line-through";
+//   }
 
-  if (Number(totalLove.innerText) >= Number(treatCost.innerText)) {
-    buyTreat.style.backgroundColor = "yellowgreen";
-  } else {
-    buyTreat.style.backgroundColor = "silver";
-  }
-}
+//   if (Number(totalLove.innerText) >= Number(treatCost.innerText)) {
+//     buyTreat.style.backgroundColor = "yellowgreen";
+//   } else {
+//     buyTreat.style.backgroundColor = "silver";
+//   }
+// }
 
 function mouseUp() {
   if (totalAccessory.innerText >= 1) {
@@ -280,95 +306,95 @@ window.addEventListener("load", () => {
     }
   });
 
-  buyWater.addEventListener("click", function () {
-    if (totalLove.innerText >= Number(waterCost.innerText)) {
-      waterDisplay.style.visibility = "visible";
-      totalWater.innerText = Number(totalWater.innerText) + 1;
-      totalLove.innerText =
-        Number(totalLove.innerText) - Number(waterCost.innerText);
-      waterCost.innerText = Math.round(
-        10 * Number(waterBuyFactor) ** Number(totalWater.innerText)
-      );
-      if (Number(totalWater.innerText) % 25 === 0) {
-        waterMultiplier += 1;
-      }
-    }
-  });
+  // buyWater.addEventListener("click", function () {
+  //   if (totalLove.innerText >= Number(waterCost.innerText)) {
+  //     waterDisplay.style.visibility = "visible";
+  //     totalWater.innerText = Number(totalWater.innerText) + 1;
+  //     totalLove.innerText =
+  //       Number(totalLove.innerText) - Number(waterCost.innerText);
+  //     waterCost.innerText = Math.round(
+  //       10 * Number(waterBuyFactor) ** Number(totalWater.innerText)
+  //     );
+  //     if (Number(totalWater.innerText) % 25 === 0) {
+  //       waterMultiplier += 1;
+  //     }
+  //   }
+  // });
 
-  buyFood.addEventListener("click", function () {
-    if (totalLove.innerText >= Number(foodCost.innerText)) {
-      foodDisplay.style.visibility = "visible";
-      totalFood.innerText = Number(totalFood.innerText) + 1;
-      totalLove.innerText =
-        Number(totalLove.innerText) - Number(foodCost.innerText);
-      foodCost.innerText = Math.round(
-        50 * Number(foodBuyFactor) ** Number(totalFood.innerText)
-      );
-      if (Number(totalFood.innerText) % 15 === 0) {
-        foodMultiplier += 1;
-      }
-    }
-  });
+  // buyFood.addEventListener("click", function () {
+  //   if (totalLove.innerText >= Number(foodCost.innerText)) {
+  //     foodDisplay.style.visibility = "visible";
+  //     totalFood.innerText = Number(totalFood.innerText) + 1;
+  //     totalLove.innerText =
+  //       Number(totalLove.innerText) - Number(foodCost.innerText);
+  //     foodCost.innerText = Math.round(
+  //       50 * Number(foodBuyFactor) ** Number(totalFood.innerText)
+  //     );
+  //     if (Number(totalFood.innerText) % 15 === 0) {
+  //       foodMultiplier += 1;
+  //     }
+  //   }
+  // });
 
-  buyBed.addEventListener("click", function () {
-    if (totalLove.innerText >= Number(bedCost.innerText)) {
-      bedDisplay.style.visibility = "visible";
-      totalBed.innerText = Number(totalBed.innerText) + 1;
-      totalLove.innerText =
-        Number(totalLove.innerText) - Number(bedCost.innerText);
-      bedCost.innerText = Math.round(
-        100 * Number(bedBuyFactor) ** Number(totalBed.innerText)
-      );
-      if (Number(totalBed.innerText) % 10 === 0) {
-        bedMultiplier += 1;
-      }
-    }
-  });
+  // buyBed.addEventListener("click", function () {
+  //   if (totalLove.innerText >= Number(bedCost.innerText)) {
+  //     bedDisplay.style.visibility = "visible";
+  //     totalBed.innerText = Number(totalBed.innerText) + 1;
+  //     totalLove.innerText =
+  //       Number(totalLove.innerText) - Number(bedCost.innerText);
+  //     bedCost.innerText = Math.round(
+  //       100 * Number(bedBuyFactor) ** Number(totalBed.innerText)
+  //     );
+  //     if (Number(totalBed.innerText) % 10 === 0) {
+  //       bedMultiplier += 1;
+  //     }
+  //   }
+  // });
 
-  buyToy.addEventListener("click", function () {
-    if (totalLove.innerText >= Number(toyCost.innerText)) {
-      toyDisplay.style.visibility = "visible";
-      totalToy.innerText = Number(totalToy.innerText) + 1;
-      totalLove.innerText =
-        Number(totalLove.innerText) - Number(toyCost.innerText);
-      toyCost.innerText = Math.round(
-        500 * Number(toyBuyFactor) ** Number(totalToy.innerText)
-      );
-      if (Number(totalToy.innerText) % 5 === 0) {
-        toyMultiplier += 1;
-      }
-    }
-  });
+  // buyToy.addEventListener("click", function () {
+  //   if (totalLove.innerText >= Number(toyCost.innerText)) {
+  //     toyDisplay.style.visibility = "visible";
+  //     totalToy.innerText = Number(totalToy.innerText) + 1;
+  //     totalLove.innerText =
+  //       Number(totalLove.innerText) - Number(toyCost.innerText);
+  //     toyCost.innerText = Math.round(
+  //       500 * Number(toyBuyFactor) ** Number(totalToy.innerText)
+  //     );
+  //     if (Number(totalToy.innerText) % 5 === 0) {
+  //       toyMultiplier += 1;
+  //     }
+  //   }
+  // });
 
-  buyAccessory.addEventListener("click", function () {
-    if (totalLove.innerText >= Number(accessoryCost.innerText)) {
-      yunaInteract.src = "./../img/yunanormals.png";
-      totalAccessory.innerText = Number(totalAccessory.innerText) + 1;
-      totalLove.innerText =
-        Number(totalLove.innerText) - Number(accessoryCost.innerText);
-      accessoryCost.innerText = Math.round(
-        1000 * Number(accessoryBuyFactor) ** Number(totalAccessory.innerText)
-      );
-      if (Number(totalAccessory.innerText) % 10 === 0) {
-        accessoryMultiplier += 1;
-      }
-    }
-  });
+  // buyAccessory.addEventListener("click", function () {
+  //   if (totalLove.innerText >= Number(accessoryCost.innerText)) {
+  //     yunaInteract.src = "./../img/yunanormals.png";
+  //     totalAccessory.innerText = Number(totalAccessory.innerText) + 1;
+  //     totalLove.innerText =
+  //       Number(totalLove.innerText) - Number(accessoryCost.innerText);
+  //     accessoryCost.innerText = Math.round(
+  //       1000 * Number(accessoryBuyFactor) ** Number(totalAccessory.innerText)
+  //     );
+  //     if (Number(totalAccessory.innerText) % 10 === 0) {
+  //       accessoryMultiplier += 1;
+  //     }
+  //   }
+  // });
 
-  buyTreat.addEventListener("click", function () {
-    if (totalLove.innerText >= Number(treatCost.innerText)) {
-      treatDisplay.style.visibility = "visible";
-      totalTreat.innerText = Number(totalTreat.innerText) + 1;
-      totalLove.innerText =
-        Number(totalLove.innerText) - Number(treatCost.innerText);
-      treatCost.innerText = Math.round(
-        5000 * Number(treatBuyFactor) ** Number(totalTreat.innerText)
-      );
-      if (Number(totalTreat.innerText) % 5 === 0) {
-        treatMultiplier += 1;
-      }
-    }
-  });
+  // buyTreat.addEventListener("click", function () {
+  //   if (totalLove.innerText >= Number(treatCost.innerText)) {
+  //     treatDisplay.style.visibility = "visible";
+  //     totalTreat.innerText = Number(totalTreat.innerText) + 1;
+  //     totalLove.innerText =
+  //       Number(totalLove.innerText) - Number(treatCost.innerText);
+  //     treatCost.innerText = Math.round(
+  //       5000 * Number(treatBuyFactor) ** Number(totalTreat.innerText)
+  //     );
+  //     if (Number(totalTreat.innerText) % 5 === 0) {
+  //       treatMultiplier += 1;
+  //     }
+  //   }
+  // });
 
   setInterval(playGame, 1000);
 
